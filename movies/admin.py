a430418c912cc8ed1e315751b9db9a0af92255fe
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2023-11-29 02:54:24 +0330

    Customized the admin panel.

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'release_year', 'genre', 'daily_rental_rate')
+    exclude = ('creation_date',)
+
+
+admin.site.register(models.Genre, GenreAdmin)
+admin.site.register(models.Movie, MovieAdmin)
