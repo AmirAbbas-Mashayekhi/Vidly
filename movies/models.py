4db04c94b27aa73eca5c7de56aa95c24a19894cc
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2023-11-29 02:55:16 +0330

    Implemented __str__ for movie models.

--- movies/models.py
+++ movies/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 class Genre(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.name
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
@@ -13,3 +16,6 @@ class Movie(models.Model):
     daily_rental_rate = models.FloatField()
     creation_date = models.DateTimeField(default=timezone.now)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
