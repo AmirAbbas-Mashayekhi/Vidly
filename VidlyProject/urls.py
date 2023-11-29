6754056841a5b39035ad91a67fae5d42d7aa0d9a
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2023-11-29 19:09:24 +0330

    Design Changes.

--- VidlyProject/urls.py
+++ VidlyProject/urls.py
@@ -22,7 +22,7 @@ from api.models import MovieResource
 movie_resource = MovieResource()
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('api/', include(movie_resource.urls)),
