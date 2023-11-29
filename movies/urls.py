8bedb16aaffbf78026f031a334a5e37e9eaed131
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2023-11-29 04:55:02 +0330

    Major Changes.

--- movies/urls.py
+++ movies/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='movies/index'),
+    path('', views.index, name='movies_index'),
+    path('<int:movie_id>', views.movie_details, name='movies_details'),
 ]
 
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from . import models
 
@@ -5,3 +6,8 @@ from . import models
 def index(request):
     movies = models.Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
+
+
+def movie_details(request, movie_id: int):
+    movie = get_object_or_404(models.Movie, id=movie_id)
+    return render(request, 'movies/movie_details.html', {'movie': movie})
