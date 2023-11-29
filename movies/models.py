fe6c2ebba99cf68ddd4d3073bd203c9a79f0b9a7
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2023-11-29 05:30:17 +0330

    Fixed Integrity Error.

--- movies/models.py
+++ movies/models.py
@@ -15,7 +15,6 @@ class Movie(models.Model):
     number_in_stock = models.IntegerField()
     daily_rental_rate = models.FloatField()
     creation_date = models.DateTimeField(default=timezone.now)
-    image = models.ImageField(upload_to='images')
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
     def __str__(self):
