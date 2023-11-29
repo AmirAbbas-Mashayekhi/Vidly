2ae6e886ddc2d22cb390037db5a67c2a52fb4166
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2023-11-29 05:17:11 +0330

    reverted some changes.

--- movies/models.py
+++ movies/models.py
@@ -15,7 +15,7 @@ class Movie(models.Model):
     number_in_stock = models.IntegerField()
     daily_rental_rate = models.FloatField()
     creation_date = models.DateTimeField(default=timezone.now)
-    image = models.ImageField(upload_to='images', blank=True)
+    image = models.ImageField(upload_to='images')
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
     def __str__(self):
