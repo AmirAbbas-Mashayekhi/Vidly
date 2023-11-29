cab1cec4d03191282f6591dc5d5af7bc274d6e9c
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2023-11-29 04:58:08 +0330

    Made migrations for imagebox.

--- movies/models.py
+++ movies/models.py
@@ -15,7 +15,7 @@ class Movie(models.Model):
     number_in_stock = models.IntegerField()
     daily_rental_rate = models.FloatField()
     creation_date = models.DateTimeField(default=timezone.now)
-    image = models.ImageField(upload_to='images')
+    image = models.ImageField(upload_to='images', blank=True)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
     def __str__(self):
